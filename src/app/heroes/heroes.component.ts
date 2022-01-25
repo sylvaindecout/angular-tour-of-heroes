0d00a7175154a09337e1018b7a13de19b1b7f8b3
Sylvain Decout <sylvain.decout+github@gmail.com>
2022-01-25 14:46:20 +0100

    :sparkles: Fetch list of heroes from dedicated service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {HEROES} from '../mock-heroes';
 import {Hero} from "../hero";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +9,14 @@ import {Hero} from "../hero";
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero: Hero): void {
