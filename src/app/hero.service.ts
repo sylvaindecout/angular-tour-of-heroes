4149ff0ca9a45521f9c84428f1262862b79b8d95
Sylvain Decout <sylvain.decout+github@gmail.com>
2022-01-25 17:02:31 +0100

    :sparkles: Save changes to in-memory web API

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,12 +4,15 @@ import {catchError, Observable, of, tap} from 'rxjs';
 
 import {Hero} from './hero';
 import {MessageService} from './message.service';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({providedIn: 'root'})
 export class HeroService {
 
   private heroesUrl = 'api/heroes';
+  private httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
 
   constructor(private http: HttpClient,
               private messageService: MessageService) {
@@ -28,6 +31,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
